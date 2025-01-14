f8a2601efd4e8a84649fa9f11f9adfd0d3b00d75
Fernando Silva Maransatto <fernando.maransatto@gmail.com>
2025-01-14 19:29:57 -0300

    feat: 575. Working with mocks

--- src/components/Async.test.js
+++ src/components/Async.test.js
@@ -5,8 +5,15 @@ import Async from "./Async";
 
 describe("Async Component", () => {
   test("renders posts if request suceeds", async () => {
+    // arrange
+    // mock the fetching function
+    window.fetch = jest.fn();
+    window.fetch.mockResolvedValueOnce({
+      json: async () => [{ id: "p1", title: "First post" }],
+    });
     render(<Async />);
 
+    // assert
     const listItemElements = await screen.findAllByRole("listitem");
     expect(listItemElements).not.toHaveLength(0);
   });
