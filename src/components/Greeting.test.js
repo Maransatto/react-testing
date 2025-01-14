708569d381a26ccc691c96baef2edc6247053fb4
Fernando Silva Maransatto <fernando.maransatto@gmail.com>
2025-01-14 18:40:07 -0300

    feat: 570. Writing Our Fist Test

---
+++ src/components/Greeting.test.js
@@ -0,0 +1,23 @@
+import { render, screen } from "@testing-library/react";
+import Greeting from "./Greeting";
+
+// 3 "a"s
+// Arrange -> Set up the test data, test conditions and test environment;
+// Act -> Run logid that should be tested (e.g. excute functions);
+// Assert -> Compare execution results with expected results;
+
+test("renders Hello World as a text", () => {
+  // Arrange
+  render(<Greeting />);
+
+  // Act
+  ///... nothing
+
+  // Assert
+  // get functions will throw an error if the elemet is not found
+  // query functions won't throw an error if the element is not found
+  // find functions will return a promise
+
+  const helloWorldElement = screen.getByText("Hello World", { exact: false });
+  expect(helloWorldElement).toBeInTheDocument();
+});
