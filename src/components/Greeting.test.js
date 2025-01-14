0cfc60824fa705f0bfa0b97d9813ffe5b5d019c0
Fernando Silva Maransatto <fernando.maransatto@gmail.com>
2025-01-14 18:42:53 -0300

    feat: 571. Grouping tests together with test suites

--- src/components/Greeting.test.js
+++ src/components/Greeting.test.js
@@ -5,19 +5,20 @@ import Greeting from "./Greeting";
 // Arrange -> Set up the test data, test conditions and test environment;
 // Act -> Run logid that should be tested (e.g. excute functions);
 // Assert -> Compare execution results with expected results;
+describe("Greeting component", () => {
+  test("renders Hello World as a text", () => {
+    // Arrange
+    render(<Greeting />);
 
-test("renders Hello World as a text", () => {
-  // Arrange
-  render(<Greeting />);
+    // Act
+    ///... nothing
 
-  // Act
-  ///... nothing
+    // Assert
+    // get functions will throw an error if the elemet is not found
+    // query functions won't throw an error if the element is not found
+    // find functions will return a promise
 
-  // Assert
-  // get functions will throw an error if the elemet is not found
-  // query functions won't throw an error if the element is not found
-  // find functions will return a promise
-
-  const helloWorldElement = screen.getByText("Hello World", { exact: false });
-  expect(helloWorldElement).toBeInTheDocument();
+    const helloWorldElement = screen.getByText("Hello World", { exact: false });
+    expect(helloWorldElement).toBeInTheDocument();
+  });
 });
