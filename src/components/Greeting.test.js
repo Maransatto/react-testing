d50db6d567b6907f76d378069fd0ed40958ceff9
Fernando Silva Maransatto <fernando.maransatto@gmail.com>
2025-01-14 18:55:43 -0300

    feat: 572. Testing Ures Interaction & State

--- src/components/Greeting.test.js
+++ src/components/Greeting.test.js
@@ -1,5 +1,6 @@
 import { render, screen } from "@testing-library/react";
 import Greeting from "./Greeting";
+import userEvent from "@testing-library/user-event";
 
 // 3 "a"s
 // Arrange -> Set up the test data, test conditions and test environment;
@@ -20,5 +21,28 @@ describe("Greeting component", () => {
 
     const helloWorldElement = screen.getByText("Hello World", { exact: false });
     expect(helloWorldElement).toBeInTheDocument();
+  });
+
+  test("renders good to see you if the button was NOT clicked", () => {
+    render(<Greeting />);
+    const outputElement = screen.getByText("It's good to see you!", {
+      exact: false,
+    });
+    expect(outputElement).toBeInTheDocument();
+  });
+
+  test('renders "Changed!" if the button was clicked', () => {
+    // arrange
+    render(<Greeting />);
+
+    // act
+    const button = screen.getByRole("button");
+    userEvent.click(button);
+
+    // assert
+    const outputElement = screen.getByText("Changed!", {
+      exact: false,
+    });
+    expect(outputElement).toBeInTheDocument();
   });
 });
